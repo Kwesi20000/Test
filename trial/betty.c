a1f68905057ddbef5ca977fb1b2a35a1a592e6f4
Kwesi20000 <hendrickskumi@yahoo.com>
2023-08-22 11:18:14 -0700

    shell.h

--- trial/betty.c
+++ trial/betty.c
@@ -1,61 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <string.h>
 
 /**
- * main - Simple shell program
+ * main - adds two numbers
  *
  * Return: Always 0
  */
+
 int main(void)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    pid_t child_pid;
-    int status;
 
-    while (1)
-    {
-        printf("($) ");
-        read = getline(&line, &len, stdin);
-        if (read == -1)
-        {
-            if (isatty(STDIN_FILENO))
-                printf("\n");
-            break;
-        }
-        if (line[read - 1] == '\n')
-            line[read - 1] = '\0';
+	int a, b, sum;
 
-        child_pid = fork();
-        if (child_pid == -1)
-        {
-            perror("Error");
-            return (EXIT_FAILURE);
-        }
-        if (child_pid == 0)
-        {
-            char *args[2]; // Assuming a simple command with no arguments
+	a = 3;
+	b = 4;
 
-            args[0] = line;
-            args[1] = NULL;
+	sum = a + b;
 
-            execve(line, args, NULL); // Execute the command
-            perror("Error");
-            free(line);
-            exit(EXIT_FAILURE);
-        }
-        else
-        {
-            wait(&status);
-        }
-    }
+	printf("%d\n", sum);
 
-    free(line);
-    return (EXIT_SUCCESS);
+	return (0);
 }
 
